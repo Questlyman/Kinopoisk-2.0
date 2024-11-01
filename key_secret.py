99fd84cb180b41bf1747dc41e4002f637c31f458
REL4T1N <129988850+REL4T1N@users.noreply.github.com>
2024-11-01 13:10:44 +0300

    Update key_secret.py

--- key_secret.py
+++ key_secret.py
@@ -1,9 +1,9 @@
 # import os
 # secret_key = os.urandom(24).hex()
 # print(secret_key)
-secret_key = '4a929ab71883582bb816cb0159a71b85f88d8f6ce0a4cb3a'
+secret_key = ''
 username = 'postgres'  #SELECT current_user; в SQL Shell
 localhost = 'localhost'  #SHOW listen_addresses;
 database = 'users'  #SELECT current_database();
-pswd = '17359'  #просто знаю его
+pswd = ''  #просто знаю его
 
