18f9142ac9bba82c009637c6e69021c1c23f3776
REL4T1N <129988850+REL4T1N@users.noreply.github.com>
2024-11-11 22:11:05 +0300

    Delete key_secret.py

--- key_secret.py
+++
@@ -1,9 +0,0 @@
-# import os
-# secret_key = os.urandom(24).hex()
-# print(secret_key)
-secret_key = ''
-username = 'postgres'  #SELECT current_user; в SQL Shell
-localhost = 'localhost'  #SHOW listen_addresses;
-database = 'users'  #SELECT current_database();
-pswd = ''  #просто знаю его
-
