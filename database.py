a9473812872f60e79c64264c3ad925312ddfd534
REL4T1N <129988850+REL4T1N@users.noreply.github.com>
2024-11-11 22:12:15 +0300

    add DB file

---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker,  declarative_base
+# from sqlalchemy.ext.declarative import declarative_base
+import os
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+Base = declarative_base()
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base.metadata.create_all(engine)
